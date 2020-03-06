57692f2399064184994c26091f94e9c6b8798809
Marc <marcmestre@gmail.com>
2020-03-06 02:51:36 -0500

    styles and adding nav slider

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// nav
+function openNav() {
+  $('#full-nav').animate({ right: '0' });
+  $('#full-nav-open').hide();
+  $('#full-nav-close').show();
+}
+function closeNav() {
+  $('#full-nav').animate({ right: '-100vw' });
+  $('#full-nav-close').hide();
+  $('#full-nav-open').show();
+}
+$( document ).ready (function() {
+  $( '#full-nav-close' ).on('click', closeNav );
+  $( '#full-nav-open' ).on('click', openNav );
+});
